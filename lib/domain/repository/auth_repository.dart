e49a6b7ad9853e3ce3e5f3319e0bfc01beef8d5c
gminiy <gmini.y@gmail.com>
2024-01-28 21:20:13 +0900

    fix: AuthRepository logout return void

--- lib/domain/repository/auth_repository.dart
+++ lib/domain/repository/auth_repository.dart
@@ -1,8 +1,8 @@
 import 'package:ev_charger_app/domain/model/user_model.dart';
 
 abstract interface class AuthRepository {
-  Future<UserModel> login();
-  Future<void> logout();
+  Future<UserModel> login(UserModel user);
+  void logout();
   Future<UserModel> update(UserModel newUser);
   Future<UserModel?> getUserModel(String id);
 }
