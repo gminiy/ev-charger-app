4eb3e5cbfcdae61d29d00213cd767371f8f8aefd
gminiy <gmini.y@gmail.com>
2024-01-29 18:32:38 +0900

    feat: 외부 클릭시 키보드 숨기기, 입력 완료 버튼 패딩 추가

--- lib/presentation/register_address/register_address_screen.dart
+++ lib/presentation/register_address/register_address_screen.dart
@@ -22,94 +22,102 @@ class _RegisterAddressScreenState extends State<RegisterAddressScreen> {
   Widget build(BuildContext context) {
     final viewModel = context.watch<RegisterAddressViewModel>();
 
-    return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              '주소를 입력해주세요.',
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                '주소를 입력해주세요.',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 32),
-            const Text(
-              '주소',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+              const SizedBox(height: 32),
+              const Text(
+                '주소',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            TextField(
-              controller: textEditingController,
-              onChanged: (value) {
-                viewModel.fetchAddresses(value);
-              },
-              decoration: InputDecoration(
-                hintText: '주소 입력',
-                suffixIcon: textEditingController.text == ''
-                    ? null
-                    : IconButton(
-                        icon: const Icon(Icons.cancel_outlined),
-                        onPressed: () {
-                          textEditingController.text = '';
-                          viewModel.fetchAddresses('');
+              TextField(
+                controller: textEditingController,
+                onChanged: (value) {
+                  viewModel.fetchAddresses(value);
+                },
+                decoration: InputDecoration(
+                  hintText: '주소 입력',
+                  suffixIcon: textEditingController.text == ''
+                      ? null
+                      : IconButton(
+                          icon: const Icon(Icons.cancel_outlined),
+                          onPressed: () {
+                            textEditingController.text = '';
+                            viewModel.fetchAddresses('');
+                          },
+                        ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: viewModel.state.isLoading
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : ListView.builder(
+                        itemCount: viewModel.state.addresses.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(
+                              title:
+                                  Text(viewModel.state.addresses[index].address),
+                              onTap: () {
+                                viewModel.selectAddress(
+                                    viewModel.state.addresses[index]);
+                                textEditingController.text =
+                                    viewModel.state.addresses[index].address;
+                              });
                         },
                       ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    width: 3,
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    width: 3,
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-                ),
               ),
-            ),
-            Expanded(
-              child: viewModel.state.isLoading
-                  ? const Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : ListView.builder(
-                      itemCount: viewModel.state.addresses.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return ListTile(
-                            title:
-                                Text(viewModel.state.addresses[index].address),
-                            onTap: () {
-                              viewModel.selectAddress(
-                                  viewModel.state.addresses[index]);
-                              textEditingController.text =
-                                  viewModel.state.addresses[index].address;
-                            });
-                      },
+              Padding(
+                padding: const EdgeInsets.only(bottom: 32.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 56,
+                  child: ElevatedButton(
+                    onPressed: viewModel.state.selected == null
+                        ? null
+                        : () {
+                            viewModel.registerAddress(viewModel.state.selected!);
+                          },
+                    child: const Text(
+                      '입력 완료',
+                      style: TextStyle(fontSize: 16),
                     ),
-            ),
-            Container(
-              width: double.infinity,
-              height: 56,
-              child: ElevatedButton(
-                onPressed: viewModel.state.selected == null
-                    ? null
-                    : () {
-                        viewModel.registerAddress(viewModel.state.selected!);
-                      },
-                child: const Text(
-                  '입력 완료',
-                  style: TextStyle(fontSize: 16),
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
