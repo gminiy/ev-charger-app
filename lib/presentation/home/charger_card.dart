49667fa09db9822469337db4639d4e9f1da150ba
gminiy <gmini.y@gmail.com>
2024-01-30 16:18:03 +0900

    feat: home screen 구현

---
+++ lib/presentation/home/charger_card.dart
@@ -0,0 +1,109 @@
+import 'package:ev_charger_app/domain/model/charger_model.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final List<String> chargerTypes = [
+  'DC 차데모',
+  'AC 완속',
+  'DC차데모+AC3상',
+  'DC콤보',
+  'DC차데모+DC콤보',
+  'DC차데모+AC3상',
+  '+DC콤보',
+  'AC3상'
+];
+
+final List<String> status = ['통신이상', '충전가능', '충전중', '운영중지', '점검중', '상태미확인'];
+
+class ChargerCard extends StatelessWidget {
+  final ChargerModel charger;
+
+  const ChargerCard({super.key, required this.charger});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2.0,
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              charger.location,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  chargerTypes[charger.chargeType],
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+                Text(
+                  _getStatusText(charger.status),
+                  style: TextStyle(
+                    fontSize: 16,
+                    color:
+                        charger.status == 1 ? Colors.green : Colors.redAccent,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            Text(
+              '출력: ${charger.output} kW',
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black54,
+              ),
+            ),
+            const SizedBox(height: 8),
+            if (charger.lastStartChargingTimestamp != null)
+              Text(
+                _getChargeTimeInfoText(charger.lastStartChargingTimestamp!,
+                    charger.lastEndChargingTimestamp),
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Colors.black54,
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _getStatusText(int statusIndex) {
+    if (statusIndex >= 0 && statusIndex < status.length) {
+      return status[statusIndex];
+    }
+    return '알 수 없음';
+  }
+
+  String _getChargeTimeInfoText(
+      int lastStartChargingTimestamp, int? lastEndChargingTimestamp) {
+    final dateFormat = DateFormat('yy.MM.dd HH:mm:ss'); // 날짜 형식 지정
+
+    if (lastEndChargingTimestamp == null ||
+        lastStartChargingTimestamp >= lastEndChargingTimestamp) {
+      DateTime startDate = DateTime.fromMillisecondsSinceEpoch(
+          lastStartChargingTimestamp * 1000);
+      return '마지막 충전 시작 시간: ${dateFormat.format(startDate)}';
+    }
+    DateTime endDate =
+        DateTime.fromMillisecondsSinceEpoch(lastEndChargingTimestamp * 1000);
+    return '마지막 충전 완료 시간: ${dateFormat.format(endDate)}';
+  }
+}
---
+++ lib/presentation/home/home_screen.dart
@@ -0,0 +1,148 @@
+import 'package:ev_charger_app/domain/model/user_model.dart';
+import 'package:ev_charger_app/presentation/home/charger_card.dart';
+import 'package:ev_charger_app/presentation/home/home_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+final _filters = [
+  "filter1",
+  "filter2333333",
+  "filter3222",
+  "filt",
+  "filter5444",
+  "filter6",
+];
+
+class _HomeScreenState extends State<HomeScreen> {
+  Widget _buildFilterCard(String title) {
+    return Card(
+      elevation: 0.0,
+      margin: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 4.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(24.0),
+        side: const BorderSide(color: Color(0xFFDEE1E4), width: 1.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+        child: Text(
+          title,
+          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() async {
+      final viewModel = context.read<HomeViewModel>();
+      await viewModel.init();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<HomeViewModel>();
+
+    Widget _buildDrawer(UserModel user) {
+      return Drawer(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      user.nickname,
+                      style: const TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  ListTile(title: const Text('내 정보'), onTap: () {}),
+                  ListTile(title: const Text('고객센터'), onTap: () {}),
+                  ListTile(title: const Text('이용 약관 및 정책'), onTap: () {}),
+                  ListTile(title: const Text('버전 정보'), onTap: () {}),
+                ],
+              ),
+            ),
+            // 로그아웃 버튼을 바닥에 고정
+            Container(
+              child: ListTile(
+                leading: const Icon(
+                  Icons.exit_to_app, // 로그아웃 아이콘
+                  color: Colors.grey, // 아이콘 색상
+                ),
+                title: const Text(
+                  '로그아웃',
+                  style: TextStyle(
+                    color: Colors.grey, // 텍스트 색상
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onTap: () async {
+                  await viewModel.logout();
+
+                  if (!mounted) return;
+
+                  context.go('/login');
+                },
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      drawer: viewModel.state.userModel == null
+          ? null
+          : _buildDrawer(viewModel.state.userModel!),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              // snap: true,
+              floating: true,
+              backgroundColor: Colors.white,
+              centerTitle: false,
+              expandedHeight: 44,
+              title: viewModel.state.userModel == null
+                  ? const Text('')
+                  : Text(viewModel.state.userModel!.address ?? 'undefined'),
+              titleTextStyle: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(30),
+                child: SizedBox(
+                  height: 40,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: _filters.map(_buildFilterCard).toList(),
+                  ),
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate(viewModel.state.chargerModels
+                  .map((e) => ChargerCard(charger: e))
+                  .toList()),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
